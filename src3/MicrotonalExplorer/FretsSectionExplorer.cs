576e8f2793efb60bc3e4897d5d0118b9a8771998
Wilckerson <wilckerson@gmail.com>
2025-04-01 02:09:23 -0700

    TODOS

--- src3/MicrotonalExplorer/FretsSectionExplorer.cs
+++ src3/MicrotonalExplorer/FretsSectionExplorer.cs
@@ -8,24 +8,29 @@ public class FretsSectionExplorer
 {
     public static void MainComputation()
     {
+        //TODO:
+        //[] Consider non-isomorphic string steps
+        //[] More points when notes from chord don't overlap at the same string
+
         //Objective: Given a EDO subset, find good combinations of notes for the strings,
         //isomorphic or not, considering the distance of target notes on the surrounding frets
         float baseRefValue = 2;
-        float baseValue = 4;
-        float eqt = 22;
-        float stringStep = 1 / (baseValue / baseRefValue);
+        float baseValue = 8;
+        float eqt = 31;
+        float stringStep = 1 / (float)(Math.Log(baseValue) / Math.Log(baseRefValue));
         int fretSpan = 5;
 
         int stringsNumber = 6;
         float toleranceInCents = 30;
         //float[] targetNotes = [1.2f, 8 / 7f];
-        var targetNotesList = new List<float>();
-        targetNotesList.Add(1);
-        targetNotesList.AddRange(Chord.Overtone7Limit_Inversion0);
-        targetNotesList.AddRange(Chord.Overtone7Limit_Inversion1);
-        targetNotesList.AddRange(Chord.Overtone7Limit_Inversion2);
-        targetNotesList.AddRange(Chord.Overtone7Limit_Inversion3);
-        float[] targetNotes = targetNotesList.ToArray();
+        var targetNotesList = new List<float[]>
+        {
+          Chord.Overtone7Limit_Inversion0,
+          Chord.Overtone7Limit_Inversion1,
+          Chord.Overtone7Limit_Inversion2,
+          Chord.Overtone7Limit_Inversion3
+        };
+        //float[] targetNotes = targetNotesList.ToArray();
 
 
         var iterationsResult = new List<IterationResult>();
@@ -33,17 +38,27 @@ public class FretsSectionExplorer
         for (int i = 0; i < numberOfIterations; i++)
         {
             var stepsByString = stringStep * (i + 1);
-            var iteration = GetIteration(
-                stringsNumber,
-                fretSpan,
+            var avgIterations = new List<IterationResult>();
+            foreach (var targetNotes in targetNotesList)
+            {
+                var iteration = GetIteration(
+                    stringsNumber,
+                    fretSpan,
+                    stepsByString,
+                    baseRefValue,
+                    toleranceInCents,
+                    targetNotes,
+                    baseValue,
+                    eqt,
+                    false);
+                avgIterations.Add(iteration);
+            }
+            var iterationAvg = new IterationResult(
+                avgIterations.Average(s => s.Points),
                 stepsByString,
-                baseRefValue,
-                toleranceInCents,
-                targetNotes,
-                baseValue,
-                eqt,
-                false);
-            iterationsResult.Add(iteration);
+                avgIterations.SelectMany(s => s.ClosestNotes)
+            );
+            iterationsResult.Add(iterationAvg);
         }
         var sortedResult = iterationsResult.OrderBy(o => o.Points).Take(3);
         //Console.WriteLine($"Best result points: {sortedResult.Points}");
@@ -66,13 +81,13 @@ public class FretsSectionExplorer
             var result = Operations.MatchTargetRatiosToScale(targetNotes, stringScale, toleranceInCents);
             closestNotes.AddRange(result.Select((s) => new StringResult(i, s.ScaleIndex - fretSpan + 1, s.ClosestScaleRatio, s.TargetScaleRatio)));
         }
-        closestNotes = closestNotes.OrderBy(o => o.TargetRatio).ToList();
+        closestNotes = closestNotes.OrderBy(o => o.StringIndex).ToList();
         var uniqueNotesCount = closestNotes.GroupBy(x => x.TargetRatio).Count();
         //var filteredResult = closestNotes.GroupBy(x => x.TargetRatio).Select(g => g.OrderBy(o => o.Distance).First());
         var pointsByUniqueTargetCounts = 100 - (100 * ((float)uniqueNotesCount / targetNotes.Length));
         var pointsByDistance = closestNotes.GroupBy(x => x.TargetRatio).Sum(g => g.Sum(s => Math.Abs(s.Distance)) / g.Count());
-        //var pointsByUniqueByString =
-        var points = pointsByUniqueTargetCounts + pointsByDistance;
+        var pointsByRepetition = closestNotes.GroupBy(x => x.StringIndex).Where(x => x.Count() >= 2).Count() * 10;
+        var points = pointsByUniqueTargetCounts + pointsByDistance + pointsByRepetition;
         var iterationResult = new IterationResult(points, stringStep, closestNotes);
 
         if (displayOutput) Console.WriteLine(iterationResult);
